cb2a988f11faccaf88e862d25f8ee47c1da24ac6
Kirill <kirill0392@gmail.com>
2024-02-15 18:30:35 +0300

    1

---
+++ returnNegative.js
@@ -0,0 +1,12 @@
+function makeNegative(num) {
+    // Code?
+    if (num == 0) {
+      return 0
+    }
+    else if (num > 0) {
+      return -num
+    }
+    else if (num < 0) {
+      return num
+    }
+  }
