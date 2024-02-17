255830f154a56754682b86b0db83c3d1c421a174
Kirill <kirill0392@gmail.com>
2024-02-17 11:38:01 +0300

    1.1

--- abbreviateTwoWordName.js
+++ abbreviateTwoWordName.js
@@ -5,3 +5,7 @@ function abbrevName(name){
     return `${upperAbbrev[0].slice(0, 1)}.${upperAbbrev[1].slice(0, 1)}`
     }
     
+// function abbrevName(name){
+//     let nameArr = name.split(' ');
+//     return (`${nameArr[0][0]}.${nameArr[1][0]}`).toUpperCase();
+//     }
