afb229ed2731ee386b40ba89a03b2cf94fbf5987
Kirill <kirill0392@gmail.com>
2024-02-18 14:28:37 +0300

    1

---
+++ areYouPlayingBanjo?.js
@@ -0,0 +1,7 @@
+function areYouPlayingBanjo(name) {
+    let lowerName = name.toLowerCase();
+    if (lowerName[0] === 'r') {
+      return `${name} ${'plays banjo'}`
+    }
+    return `${name} ${'does not play banjo'}`;
+  }
