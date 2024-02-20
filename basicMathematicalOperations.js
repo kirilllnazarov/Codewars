b81f0d0069c4cdbdf85738e4e8253e2a4bce10ec
Kirill <kirill0392@gmail.com>
2024-02-20 11:32:16 +0300

    1.1

--- basicMathematicalOperations.js
+++ basicMathematicalOperations.js
@@ -13,3 +13,19 @@ function basicOp(operation, value1, value2)
       return value1/value2;
     }
   }
+
+// second solution
+function basicOp(operation, value1, value2) {
+    switch (operation) {
+    case '+':
+    return value1 + value2;
+    case '-':
+    return value1 - value2;
+    case '*':
+    return value1 * value2;
+    case '/':
+    return value1 / value2;
+    default:
+    return 'Please input correct operator';
+    }
+    }
