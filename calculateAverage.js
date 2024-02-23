328a47529efed0842b4f51e5b39ca063c9a3813d
Kirill <kirill0392@gmail.com>
2024-02-23 12:26:17 +0300

    1

---
+++ calculateAverage.js
@@ -0,0 +1,20 @@
+function findAverage(array) {
+    let result = 0;
+    let sum = 0;
+    
+    for (let i=0; i < array.length; i++) {
+      if (array.length === 0) {
+        result = 0;
+      }
+      else {
+        sum += array[i];
+      }
+        result = sum / array.length
+    }
+  
+    
+    return result;
+  }
+
+  //secind solution
+  
