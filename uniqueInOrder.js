63ac4f5843c9559f3fbb6085d26ff03f436bc006
Kirill <kirill0392@gmail.com>
2024-03-07 16:39:19 +0300

    1

--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -7,3 +7,10 @@ var uniqueInOrder = function (iterable) {
   }
   return result;
 };
+
+//second solution
+var uniqueInOrder = function (iterable) {
+  if (typeof iterable === "string")
+    return iterable.split("").filter((v, i, arr) => arr[i] != arr[i + 1]);
+  return iterable.filter((v, i, arr) => arr[i] != arr[i + 1]);
+};
