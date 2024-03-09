0d2ccd5cfbc190ccd1f895919eca7e8ff56d5e18
Kirill <kirill0392@gmail.com>
2024-03-09 10:58:34 +0300

    1

---
+++ catYearsDogYears.js
@@ -0,0 +1,5 @@
+var humanYearsCatYearsDogYears = function (humanYears) {
+  if (humanYears <= 2)
+    return [humanYears, 6 + 9 * humanYears, 6 + 9 * humanYears];
+  else return [humanYears, 16 + 4 * humanYears, 14 + 5 * humanYears];
+};
