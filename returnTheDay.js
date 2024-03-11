0d1241edced713db26af6a9bd4dbe8e50f5ad3c2
Kirill <kirill0392@gmail.com>
2024-03-11 14:47:53 +0300

    1

---
+++ returnTheDay.js
@@ -0,0 +1,15 @@
+function whatday(num) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  if (num < 1 || num > 7) {
+    return "Wrong, please enter a number between 1 and 7";
+  }
+  return days[num - 1];
+}
