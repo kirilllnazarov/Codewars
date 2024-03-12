af166c44ccef06245f1a13157999c6fbcf7e00af
Kirill <kirill0392@gmail.com>
2024-03-12 16:47:08 +0300

    1

---
+++ defineCardSuit.js
@@ -0,0 +1,9 @@
+function defineSuit(card) {
+  return card[card.length - 1] === "♣"
+    ? "clubs"
+    : card[card.length - 1] === "♦"
+    ? "diamonds"
+    : card[card.length - 1] === "♥"
+    ? "hearts"
+    : "spades";
+}
