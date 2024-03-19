7d9474dcaa54df005c6b1d7f10d42016164b338c
Kirill <kirill0392@gmail.com>
2024-03-20 01:10:58 +0300

    1

---
+++ fixmeGetFullName.js
@@ -0,0 +1,9 @@
+class Dinglemouse {
+  constructor(firstName, lastName) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+  getFullName() {
+    return `${this._firstName} ${this._lastName}`.trim();
+  }
+}
