9cd9d2368ff765560b86f7603a50c9a459e61709
Kirill <kirill0392@gmail.com>
2024-03-22 20:03:34 +0300

    1.1

--- sumStringsAsNumbers.js
+++ sumStringsAsNumbers.js
@@ -7,3 +7,14 @@ function sumStrings(a,b) {
     while ( b.length < a.length ){
       b = '0' + b ;
     }
+    a = a.split('').reverse().join('');
+    b = b.split('').reverse().join('');
+    var c = '' , i = 0 ;
+    var carry = 0 ;
+    for ( var i = 0 ; i < a.length ; i ++ ) {
+      var result = parseInt(a[i]) + parseInt(b[i]) + carry ;
+      carry = 0 ;
+      if ( result >= 10 ){
+        result -= 10 ;
+        carry ++ ;
+      }
