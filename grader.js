43f4c33a7743ac8766e4f094797581f58f337d89
Kirill <kirill0392@gmail.com>
2024-03-31 16:46:15 +0300

    1

---
+++ grader.js
@@ -0,0 +1,13 @@
+function grader(score) {
+	return score > 1 || score < 0.6
+		? "F"
+		: score === 0.6
+		? "D"
+		: score < 0.7
+		? "D"
+		: score < 0.8
+		? "C"
+		: score < 0.9
+		? "B"
+		: "A";
+}
