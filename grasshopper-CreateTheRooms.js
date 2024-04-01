2891e31684f587d6a1a71475c07250bb932c12ec
Kirill <kirill0392@gmail.com>
2024-04-01 20:38:59 +0300

    1

--- grasshopper-CreateTheRooms.js
+++ grasshopper-CreateTheRooms.js
@@ -1,5 +1,17 @@
 var rooms = {
-    room1:{
-     name: 1, 
-      description:2, 
-      completed:3},
+	room1: {
+		name: 1,
+		description: 2,
+		completed: 3,
+	},
+	room2: {
+		name: 1,
+		description: 2,
+		completed: 3,
+	},
+	room3: {
+		name: 1,
+		description: 2,
+		completed: 3,
+	},
+};
