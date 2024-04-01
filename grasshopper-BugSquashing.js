17f4bc4f98b46710effb58ec31601964ccd2f1f9
Kirill <kirill0392@gmail.com>
2024-04-01 20:37:42 +0300

    1

--- grasshopper-BugSquashing.js
+++ grasshopper-BugSquashing.js
@@ -1,3 +1,11 @@
-var health = 100
-var position = 0
-var coins = 0
+var health = 100;
+var position = 0;
+var coins = 0;
+function main() {
+	rollDice();
+	move();
+	combat();
+	getCoins();
+	buyHealth();
+	printStatus();
+}
