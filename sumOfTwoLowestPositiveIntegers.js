5226aff13e7035626d250efcb2f873faf7c57247
Kirill <kirill0392@gmail.com>
2024-04-06 17:49:47 +0300

    1

--- sumOfTwoLowestPositiveIntegers.js
+++ sumOfTwoLowestPositiveIntegers.js
@@ -1,2 +1,4 @@
-function sumTwoSmallestNumbers(numbers) {  
-    let sort = numbers.sort((a, b) => a - b);
+function sumTwoSmallestNumbers(numbers) {
+	let sort = numbers.sort((a, b) => a - b);
+	return sort[0] + sort[1];
+}
