da5ff0a741855d421de8a0534ed7d21f290f68d3
Kirill <kirill0392@gmail.com>
2024-04-06 17:50:32 +0300

    1

--- creditCardMask.js
+++ creditCardMask.js
@@ -1,4 +1,7 @@
 function maskify(cc) {
-    if (cc.length <= 3) {
-      return cc;
-    }
+	if (cc.length <= 3) {
+		return cc;
+	} else {
+		return `${cc.slice(0, -4).replace(/\w/g, "#")}${cc.slice(-4)}`;
+	}
+}
