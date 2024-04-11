5a08bb1305afce6d04b1856c05ca1f40dc812668
Kirill <kirill0392@gmail.com>
2024-04-11 17:21:14 +0300

    1

--- what'sUpNext?.js
+++ what'sUpNext?.js
@@ -1,15 +1,3 @@
 function nextItem(xs, item) {
-	let found = false;
-	let finalValue;
-
-	for (let i of xs) {
-		if (found) {
-			finalValue = i;
-			break;
-		}
-
-		if (i === item) found = true;
-	}
-
-	return finalValue;
+	
 }
