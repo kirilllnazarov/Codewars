0b0ed9c6835f7f72fa319841aace7bdfa830a80d
Kirill <kirill0392@gmail.com>
2024-04-12 17:45:53 +0300

    1

--- oddOrEven?.js
+++ oddOrEven?.js
@@ -1,3 +1,7 @@
 function oddOrEven(array) {
- 
-    }
+	if (array.reduce((a, b) => a + b, 0) % 2 === 0) {
+		return "even";
+	} else if (array.reduce((a, b) => a + b) % 2 !== 0) {
+		return "odd";
+	}
+}
