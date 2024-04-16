f9fd9d71855eef66ee13cadad6d01a28336cfe4c
Kirill <kirill0392@gmail.com>
2024-04-16 12:41:18 +0300

    1

--- aspectRatioCropping-Part2.js
+++ aspectRatioCropping-Part2.js
@@ -1,5 +1,23 @@
 function aspectRatio(x,y,constant){
     let [newX, newY] = [x, y];
     const ratio = 16/9;
-
+    switch(constant) {
+      case 'height':
+        newX = y*ratio;
+        break;
+      case 'width':
+        newY = x/ratio;
+        break
+      case 'diagonal':
+        const diagonal = Math.sqrt(x**2 + y**2);
+        const theta = Math.atan(ratio);
+        newX = diagonal*Math.sin(theta);
+        newY = diagonal*Math.cos(theta);
+        break;
+      case 'area':
+        const area = x * y;
+        newX = Math.sqrt(area*ratio)
+        newY = area/newX;
+    }
+  
   }
