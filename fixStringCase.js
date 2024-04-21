558e93787f6da9727cbc20943c501b158efbc20d
Kirill <kirill0392@gmail.com>
2024-04-21 13:03:15 +0300

    1

--- fixStringCase.js
+++ fixStringCase.js
@@ -1,5 +1,5 @@
 function solve(s) {
 	let up = s.split("").filter((el) => el.match(/[A-Z]/)).length;
 	let down = s.split("").filter((el) => el.match(/[a-z]/)).length;
-
+	return down >= up ? s.toLowerCase() : s.toUpperCase();
 }
