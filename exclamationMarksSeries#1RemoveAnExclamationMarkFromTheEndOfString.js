dcfc5e2719f519c2712f2343e5ba3f1cffa83a65
Kirill <kirill0392@gmail.com>
2024-04-23 21:58:14 +0300

    1

--- exclamationMarksSeries#1RemoveAnExclamationMarkFromTheEndOfString.js
+++ exclamationMarksSeries#1RemoveAnExclamationMarkFromTheEndOfString.js
@@ -1,5 +1,10 @@
-function remove(s){
-    if(s[s.length-1] == '!'){
-    return s.split('').slice(0,s.split('').length-1).join('');
-    } 
-    }
+function remove(s) {
+	if (s[s.length - 1] == "!") {
+		return s
+			.split("")
+			.slice(0, s.split("").length - 1)
+			.join("");
+	} else {
+		return s;
+	}
+}
