e7f57ac8d29211bdebfcd866acf20e9ba62a872e
Kirill <kirill0392@gmail.com>
2024-04-26 20:38:01 +0300

    1

--- checkTheExam.js
+++ checkTheExam.js
@@ -1,19 +1,16 @@
 function checkExam(array1, array2) {
-    let count = 0;
-    
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        count += 4
-      }
-       else if (array2[i] === "") {
-        count += 0
-      }
-     else if (array1[i] !== array2[i]) {
-        count --
-      }
-    }
-    if (count < 0) {
-        return 0;
-      }
-
-    }
+	let count = 0;
+	for (let i = 0; i < array1.length; i++) {
+		if (array1[i] === array2[i]) {
+			count += 4;
+		} else if (array2[i] === "") {
+			count += 0;
+		} else if (array1[i] !== array2[i]) {
+			count--;
+		}
+	}
+	if (count < 0) {
+		return 0;
+	}
+	return count;
+}
