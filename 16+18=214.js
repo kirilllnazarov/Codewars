26c0d4c0d24f592c2c0b7924a298be80c31e2d96
Kirill <kirill0392@gmail.com>
2024-04-29 23:30:07 +0300

    1

--- 16+18=214.js
+++ 16+18=214.js
@@ -1,5 +1,7 @@
 function add(num1, num2) {
     num1 = ('' + num1).split('').reverse()
     num2 = ('' + num2).split('').reverse()
+    let max = Math.max(num1.length, num2.length)
+    let str = [];
 
   }
