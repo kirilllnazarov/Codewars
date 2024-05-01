a576c1273635c079406d2da7699fa6a9b5c224a7
Kirill <kirill0392@gmail.com>
2024-05-02 00:26:26 +0300

    1

--- orderedCountOfCharacters.js
+++ orderedCountOfCharacters.js
@@ -1,11 +1,12 @@
 let orderedCount = function (text) {
-    const split = text.split('');
-    const str = [...new Set(split)];
-    
-    let result = [];
-    
-    for (let i = 0; i <= str.length-1; i += 1) {
-      result.push([str[i], split.filter((s) => s === str[i]).length]);
-    }
- 
-  }
+	const split = text.split("");
+	const str = [...new Set(split)];
+
+	let result = [];
+
+	for (let i = 0; i <= str.length - 1; i += 1) {
+		result.push([str[i], split.filter((s) => s === str[i]).length]);
+	}
+
+	return result;
+};
