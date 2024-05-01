871c3a58c31c79c50efdfce60479f9f66982675d
Kirill <kirill0392@gmail.com>
2024-05-01 23:07:50 +0300

    1

--- maximumTripletSum(ArraySeries#7).js
+++ maximumTripletSum(ArraySeries#7).js
@@ -1,6 +1,6 @@
-function maxTriSum(numbers){
-    return [...new Set(numbers)]
-      .sort( (a,b) => a - b)
-      .slice(-3)
-
-  }
+function maxTriSum(numbers) {
+	return [...new Set(numbers)]
+		.sort((a, b) => a - b)
+		.slice(-3)
+		.reduce((acc, el) => acc + el);
+}
