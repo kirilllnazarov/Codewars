0bbceeb6f0402053fc6fdf579486e3a8f4bc541a
Kirill <kirill0392@gmail.com>
2024-05-02 00:26:19 +0300

    1

--- orderedCountOfCharacters.js
+++ orderedCountOfCharacters.js
@@ -4,4 +4,8 @@ let orderedCount = function (text) {
     
     let result = [];
     
+    for (let i = 0; i <= str.length-1; i += 1) {
+      result.push([str[i], split.filter((s) => s === str[i]).length]);
+    }
+ 
   }
