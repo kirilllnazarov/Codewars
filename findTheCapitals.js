ba359948a6cd02cbbfa8e9cc1b91f3de67424f7b
Kirill <kirill0392@gmail.com>
2024-05-11 17:59:25 +0300

    1

--- findTheCapitals.js
+++ findTheCapitals.js
@@ -2,7 +2,7 @@ var capitals = function (word) {
     let resultArr = []
     for (let i = 0; i < word.length; i++) {
       if (word[i] !== word[i].toLowerCase()) {
-    
+        resultArr.push(i)
       }
     }
 
