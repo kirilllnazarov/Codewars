006d5da8754d650bf1b4f380a8de032a128ceaa8
Kirill <kirill0392@gmail.com>
2024-05-14 23:46:27 +0300

    1

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,5 +3,8 @@ function fizzbuzz(n){
      for (let i = 1; i <= n; i++) {
        if(i % 3 === 0 && i % 5 === 0) {
          arr.push("FizzBuzz")
-      
+       }else if(i % 3 === 0) {
+         arr.push("Fizz")
+       }
+    
    }
