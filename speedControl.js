bf889a589c010fa92b1af1fc92d1eb3ab800d152
Kirill <kirill0392@gmail.com>
2024-05-17 23:26:52 +0300

    1

--- speedControl.js
+++ speedControl.js
@@ -1,10 +1,10 @@
 function gps(s, x) {
-    let arr = []
-    if ( x.length <= 1 ) {
-      return 0
-    }
-    for ( i = 0; i < x.length - 1; i++ ) { 
-      arr.push( ( x[i + 1] - x[i] ) * 3600 / s );
-    }
-
-  }
+	let arr = [];
+	if (x.length <= 1) {
+		return 0;
+	}
+	for (i = 0; i < x.length - 1; i++) {
+		arr.push(((x[i + 1] - x[i]) * 3600) / s);
+	}
+	return Math.max(...arr);
+}
