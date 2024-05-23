04c2e15f7cbbdd9d75949780febe4e6477c230e5
Kirill <kirill0392@gmail.com>
2024-05-23 21:34:27 +0300

    1

--- predictYourAge!.js
+++ predictYourAge!.js
@@ -1,4 +1,6 @@
-function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
-    let Age = [age1, age2, age3, age4, age5, age6, age7, age8].map(a=>a**2).reduce((a,b)=>a+b);
-
-    }
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+	let Age = [age1, age2, age3, age4, age5, age6, age7, age8]
+		.map((a) => a ** 2)
+		.reduce((a, b) => a + b);
+	return Math.floor(Math.sqrt(Age) / 2);
+}
