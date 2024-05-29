5fef3d87e1cb0adc2450e290bba7b64255d8c0ce
Kirill <kirill0392@gmail.com>
2024-05-29 16:52:15 +0300

    1

--- removeAnchorFromUrl.js
+++ removeAnchorFromUrl.js
@@ -1,4 +1,7 @@
 function removeUrlAnchor(url) {
 	let result = "";
-	for (let i = 0; i < url.length; i++) 
+	for (let i = 0; i < url.length; i++) {
+		if (url[i] !== "#") {
+			result += url[i];
+		} 
 }
