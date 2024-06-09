a7478c9f40d60e117dc3f0bf9c09972bdd4492dd
Kirill <kirill0392@gmail.com>
2024-06-09 22:15:09 +0300

    1

--- largest5DigitNumberInAseries.js
+++ largest5DigitNumberInAseries.js
@@ -3,6 +3,9 @@ function solution(digits) {
 
 	for (let i = 0; i < digits.length; i++) {
 		let number = digits.substr(i, 5);
+		if (Number(number) > answer) {
+			answer = Number(number);
+		}
 	}
 
 }
