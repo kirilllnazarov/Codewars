430fad11646b3d1697a285361af3c3bad9208454
Kirill <kirill0392@gmail.com>
2024-06-11 22:48:43 +0300

    1

--- twoFighters,OneWinner..js
+++ twoFighters,OneWinner..js
@@ -1,4 +1,5 @@
 function declareWinner(fighter1, fighter2, firstAttacker) {
 	const f1 = Math.ceil(fighter1.health / fighter2.damagePerAttack);
+	const f2 = Math.ceil(fighter2.health / fighter1.damagePerAttack);
 
 }
