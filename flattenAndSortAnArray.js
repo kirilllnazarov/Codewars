60960531cccea4e9dacb819c7cfb4b318126a9b4
Kirill <kirill0392@gmail.com>
2024-06-13 18:10:45 +0300

    1

--- flattenAndSortAnArray.js
+++ flattenAndSortAnArray.js
@@ -3,3 +3,14 @@ function flattenAndSort(array) {
 }
 
 //second solution
+
+function flattenAndSort(array) {
+	var newArray = [];
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < array[i].length; j++) {
+			newArray.push(array[i][j]);
+		}
+	}
+
+	return newArray.sort((a, b) => a - b);
+}
