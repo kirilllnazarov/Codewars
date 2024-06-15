5cfba094bd4ccc8650d158a16b13df7a2c975172
Kirill <kirill0392@gmail.com>
2024-06-15 20:58:50 +0300

    1

--- unluckyDays.js
+++ unluckyDays.js
@@ -2,5 +2,9 @@ function unluckyDays(year) {
 	let date = new Date(year, 0).getTime();
 	let d = 1000 * 60 * 60 * 24;
 	let friday = 0;
-	
-}
+	for (let i = 0; i < 365; i++) {
+		date += d;
+		if (new Date(date).getDay() === 5 && new Date(date).getDate() === 13)
+			friday++;
+	}
+ }
