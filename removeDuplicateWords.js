c9ea7998badc13f301341a843df3705b14edbfc0
Kirill <kirill0392@gmail.com>
2024-06-19 21:51:17 +0300

    1

--- removeDuplicateWords.js
+++ removeDuplicateWords.js
@@ -1,3 +1,9 @@
-function removeDuplicateWords (s) {
-
-  }
+function removeDuplicateWords(s) {
+	let sentence = s.split(" ");
+	let res = [];
+	for (let i = 0; i < sentence.length; i++) {
+		console.log(sentence.indexOf(sentence[i]));
+		if (sentence.indexOf(sentence[i]) === i) res.push(sentence[i]);
+	}
+	return res.join(" ");
+}
