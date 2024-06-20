263b4f9e19f6f2672f8c1ec0c5b27cc9f13aa972
Kirill <kirill0392@gmail.com>
2024-06-20 21:03:01 +0300

    1

--- removeDuplicateWords.js
+++ removeDuplicateWords.js
@@ -14,7 +14,9 @@ function removeDuplicateWords(s) {
 	let result = [];
 	for (let i = 0; i < s.length; i++) {
 		for (let j = 0; j < s.length; j++) {
-		
+			if (result.includes(s[i])) {
+				continue;
+			} 
 		}
 	}
 
