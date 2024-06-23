74fdf420522aba38fef8c4c53e3b6b0f4da2802b
Kirill <kirill0392@gmail.com>
2024-06-23 13:44:55 +0300

    1

--- trainingJs#4:BasicDataTypes--Array.js
+++ trainingJs#4:BasicDataTypes--Array.js
@@ -1,4 +1,8 @@
-function getLength(arr){
-    //return length of arr
-    return arr.length
-  }
+function getLength(arr) {
+	//return length of arr
+	return arr.length;
+}
+function getFirst(arr) {
+	//return the first element of arr
+	return arr[0];
+}
