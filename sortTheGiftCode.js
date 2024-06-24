00f17d20554a3ee9c58393f55807b8f102667e91
Kirill <kirill0392@gmail.com>
2024-06-24 23:44:40 +0300

    1

--- sortTheGiftCode.js
+++ sortTheGiftCode.js
@@ -5,5 +5,7 @@ function sortGiftCode(code) {
 //second solution
 function sortGiftCode(code) {
 	let result = "";
-	
-}
+	for (let i = 0; i < code.length; i++) {
+		result = `${code[i]}${result}`;
+	}
+ }
