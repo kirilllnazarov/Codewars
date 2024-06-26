09694ba4ecf48f4985c1e593aad9fcfe3656e18f
Kirill <kirill0392@gmail.com>
2024-06-26 18:46:45 +0300

    1

--- loveVsFriendship.js
+++ loveVsFriendship.js
@@ -1,4 +1,7 @@
 function wordsToMarks(string) {
 	let letterSum = 0;
-	for (let i = 0; i < string.length; i++) 
+	for (let i = 0; i < string.length; i++) {
+		letterSum = letterSum + string.charCodeAt(i) - 96;
+	}
+
 }
