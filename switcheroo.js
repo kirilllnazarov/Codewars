4eaea53d4edb6ada7d5588504b2f13903b7f9fe6
Kirill <kirill0392@gmail.com>
2024-07-03 17:21:28 +0300

    1

---
+++ switcheroo.js
@@ -0,0 +1,13 @@
+function switcheroo(x) {
+	let result = "";
+	for (let i = 0; i < x.length; i++) {
+		if (x[i] === "a") {
+			result += "b";
+		} else if (x[i] === "b") {
+			result += "a";
+		} else {
+			result += x[i];
+		}
+	}
+	return result;
+}
