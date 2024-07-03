7db22ca44ed92ec8c43545e99acee6564692010c
Kirill <kirill0392@gmail.com>
2024-07-03 14:53:45 +0300

    1

--- digitsExplosion.js
+++ digitsExplosion.js
@@ -6,3 +6,12 @@ function explode(s) {
 }
 
 //second solution
+function explode(s) {
+	let result = "";
+	for (let i = 0; i < s.length; i++) {
+		for (let j = s[i]; j > 0; j--) {
+			result += s[i];
+		}
+	}
+	return result;
+}
