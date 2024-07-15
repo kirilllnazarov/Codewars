d182a02e16d68d2b08bbf6716837bc90acd04f55
Kirill <kirill0392@gmail.com>
2024-07-15 14:41:23 +0300

    1

--- trainingJs#10:LoopStatement--For.js
+++ trainingJs#10:LoopStatement--For.js
@@ -1,6 +1,9 @@
 function pickIt(arr) {
 	let odd = [];
 	let even = [];
-	for (let i = 0; i < arr.length; i++) 
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % 2 === 0) {
+			even.push(arr[i]);
+		}
 	return [odd, even];
 }
