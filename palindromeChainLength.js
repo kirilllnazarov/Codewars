f949e243e794d7e127f8ac1ac390c709465b5b26
Kirill <kirill0392@gmail.com>
2024-07-18 19:54:20 +0300

    1

--- palindromeChainLength.js
+++ palindromeChainLength.js
@@ -1,3 +1,11 @@
 var palindromeChainLength = function (n) {
+	var count = 0;
+	var palindrome = parseInt((n + "").split("").reverse().join(""));
+	while (palindrome !== n) {
+		n = parseInt(n) + parseInt(palindrome);
+		palindrome = parseInt((n + "").split("").reverse().join(""));
+		++count;
+	}
 
-  };
+	return count;
+};
