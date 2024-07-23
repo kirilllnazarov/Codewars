642177552b3ec47d5731f42efcb3fbd78fd9bc28
Kirill <kirill0392@gmail.com>
2024-07-23 21:34:03 +0300

    1

---
+++ funWithEs6Classes#2-AnimalAndInheritance.js
@@ -0,0 +1,12 @@
+class Shark extends Animal {
+	constructor(name, age, status) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.legs = 0;
+		this.status = status;
+		this.species = "shark";
+	}
+}
+
+
