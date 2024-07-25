42a3aca4c058833390fb98e8fd3e966189f0cdab
Kirill <kirill0392@gmail.com>
2024-07-25 23:30:19 +0300

    1

--- catsAndShelves.js
+++ catsAndShelves.js
@@ -1,14 +1,12 @@
 function solution(start, finish) {
-
-    if(start === finish) {
-        return 0;
-    }
-    let now = start;
-    let sum = 0;
-    while((finish - now) >= 3) {   
-        now += 3;
-        sum ++;
-    }
-
+	if (start === finish) {
+		return 0;
+	}
+	let now = start;
+	let sum = 0;
+	while (finish - now >= 3) {
+		now += 3;
+		sum++;
+	}
+	return sum + ((finish - now) % 3);
 }
-
