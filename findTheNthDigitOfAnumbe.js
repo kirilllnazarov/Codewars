411a352cceea9c968cc0df43c8fdefbf2037179a
Kirill <kirill0392@gmail.com>
2024-08-08 13:44:55 +0300

    1

--- findTheNthDigitOfAnumbe.js
+++ findTheNthDigitOfAnumbe.js
@@ -1,9 +1,11 @@
-function findDigit(num, nth){
-    if(nth <= 0) return -1
-    else {
-        num = Math.abs(num).toString().split('').reverse()
-        if(num.length+1 <= nth){
-            return 0
-        } 
-    }
+function findDigit(num, nth) {
+	if (nth <= 0) return -1;
+	else {
+		num = Math.abs(num).toString().split("").reverse();
+		if (num.length + 1 <= nth) {
+			return 0;
+		} else {
+			return Number(num[nth - 1]);
+		}
+	}
 }
