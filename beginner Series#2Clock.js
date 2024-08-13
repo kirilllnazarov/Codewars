2a43579dc876186956c2d6f5def2cc2e20aeda79
Kirill <kirill0392@gmail.com>
2024-08-13 21:48:11 +0300

    1

--- beginner Series#2Clock.js
+++ beginner Series#2Clock.js
@@ -1,6 +1,6 @@
-function past(h, m, s){
-    h = h * 3600000
-    m = m * 60000
-    s = s * 1000
-    return h + m + s
-    }
+function past(h, m, s) {
+	h = h * 3600000;
+	m = m * 60000;
+	s = s * 1000;
+	return h + m + s;
+}
