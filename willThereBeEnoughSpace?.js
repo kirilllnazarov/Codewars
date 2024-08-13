f6059c7c09f899110d2511a96d9a6c5007359f10
Kirill <kirill0392@gmail.com>
2024-08-13 22:22:22 +0300

    1

--- willThereBeEnoughSpace?.js
+++ willThereBeEnoughSpace?.js
@@ -1,3 +1,13 @@
 function enough(cap, on, wait) {
 	return cap - on >= wait ? 0 : on + wait - cap;
 }
+
+//second solution
+function enough(cap, on, wait) {
+	switch (true) {
+		case cap - on >= wait:
+			return 0;
+		case cap - on <= wait:
+			return on + wait - cap;
+	}
+}
