56f6a159f0c0125ee6712c906a618ed64e7fc3e0
Kirill <kirill0392@gmail.com>
2024-08-18 19:47:15 +0300

    1

--- alternatingCase.js
+++ alternatingCase.js
@@ -1,7 +1,9 @@
 String.prototype.toAlternatingCase = function () {
 	let str = "";
 	for (let i = 0; i < this.length; i++) {
-		 
+		if (this[i] === this[i].toLowerCase()) {
+			str += this[i].toUpperCase();
+		}  
 	}
 	return str;
 };
