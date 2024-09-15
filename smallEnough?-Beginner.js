3880a4821a15fe7c0007dd918891e030555ea510
Kirill <kirill0392@gmail.com>
2024-09-15 21:37:46 +0300

    1

--- smallEnough?-Beginner.js
+++ smallEnough?-Beginner.js
@@ -1,3 +1,7 @@
 function smallEnough(a, limit) {
 	return a.filter((el) => el > limit).length > 0 ? false : true;
 }
+
+//second solution
+const smallEnough = (a, limit) =>
+	a.filter((el) => el > limit).length > 0 ? false : true;
