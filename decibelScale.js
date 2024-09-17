f8d9e7c661421625fb5772369862a66fbc77d458
Kirill <kirill0392@gmail.com>
2024-09-17 22:09:30 +0300

    1

--- decibelScale.js
+++ decibelScale.js
@@ -1,3 +1,6 @@
 function dBScale(intensity) {
 	return 10 * Math.log10(intensity / Math.pow(10, -12));
 }
+
+//second solution
+const dBScale = (intensity) => 10 * Math.log10(intensity / Math.pow(10, -12));
