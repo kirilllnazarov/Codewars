8b321b610f2341868602d33798a99c5bc6ba9611
Kirill <kirill0392@gmail.com>
2024-09-25 22:57:04 +0300

    1

--- easylogs.js
+++ easylogs.js
@@ -1,3 +1,6 @@
 function logs(x, a, b) {
-  return Math.log(a) / Math.log(x) + Math.log(b) / Math.log(x);
+	return Math.log(a) / Math.log(x) + Math.log(b) / Math.log(x);
 }
+
+//second solution
+const logs = (x, a, b) => Math.log(a) / Math.log(x) + Math.log(b) / Math.log(x);
