42638c737be844038c98ee7023dd8b26891405b7
Kirill <kirill0392@gmail.com>
2024-10-15 19:07:20 +0300

    1

--- evilOrOdious.js
+++ evilOrOdious.js
@@ -1,3 +1,6 @@
 function evil(n) {
   return n.toString(2).match(/1/g).length % 2 ? "It's Odious!" : "It's Evil!";
 }
+
+//second solution
+const evil = (n) => n.toString(2).match(/1/g).length % 2 ? "It's Odious!" : "It's Evil!";
