e06f93350913dcbe18c34cc8b1c9319de70ce2fc
Kirill <kirill0392@gmail.com>
2024-10-16 21:07:44 +0300

    1

--- fixTheBugs(Syntax)-MyFirstKata.js
+++ fixTheBugs(Syntax)-MyFirstKata.js
@@ -1,7 +1,10 @@
 function myFirstKata(a, b) {
-  if (typeof a !== "number" || typeof b !== "number") {
-    return false;
-  } else {
-    return (a % b) + (b % a);
-  }
+	if (typeof a !== "number" || typeof b !== "number") {
+		return false;
+	} else {
+		return (a % b) + (b % a);
+	}
 }
+
+//second solution
+const myFirstKata = (a, b) => (typeof a !== "number" || typeof b !== "number" ? false : (a % b) + (b % a));
