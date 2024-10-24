9823af49c037018a731e739a2262d5e2e5872b83
Kirill <kirill0392@gmail.com>
2024-10-24 21:56:18 +0300

    1

--- mostDigits.js
+++ mostDigits.js
@@ -1,3 +1,6 @@
 function findLongest(array) {
 	return array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
 }
+
+//second solution
+const findLongest = (array) => array.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a));
