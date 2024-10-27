157ef975649401ad0c51ac6333c1ac44a2adeb38
Kirill <kirill0392@gmail.com>
2024-10-27 17:24:26 +0300

    1

--- mumbling.js
+++ mumbling.js
@@ -1,6 +1,13 @@
 function accum(s) {
-  return s
-    .split("")
-    .map((el, i) => el.toUpperCase() + el.toLowerCase().repeat(i))
-    .join("-");
+	return s
+		.split("")
+		.map((el, i) => el.toUpperCase() + el.toLowerCase().repeat(i))
+		.join("-");
 }
+
+//second solution
+const accum = (s) =>
+	s
+		.split("")
+		.map((el, i) => el.toUpperCase() + el.toLowerCase().repeat(i))
+		.join("-");
